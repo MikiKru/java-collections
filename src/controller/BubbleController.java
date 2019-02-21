3f828945c2f46ef1c96a5161a5e27f361599d264
MikiKru <michal_kruczkowski@o2.pl>
2019-02-21 17:48:19 +0100

    BubbleSort ASC/DESC

---
+++ src/controller/BubbleController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BubbleController {
+    List<Integer> list = new ArrayList<>();
+
+    public BubbleController(List<Integer> list) {
+        this.list = list;
+    }
+
+    public List bubbleSort(String type) {
+        // pętla iterująca po testach
+        for (int j = 1; j < list.size(); j++) {
+            // pętla iterująca po wartościach
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i - 1) < list.get(i) && type.toUpperCase().equals("DESC")) {
+                        swap(i - 1, i);
+                }
+                if (list.get(i - 1) > list.get(i) && type.toUpperCase().equals("ASC")) {
+                    swap(i - 1, i);
+                }
+            }
+        }
+        return list;
+    }
+    public void swap(int i1, int i2) {
+        int tmp = list.get(i1);
+        list.set(i1, list.get(i2));
+        list.set(i2, tmp);
+    }
+    public static void main(String[] args) {
+        BubbleController bc = new BubbleController(new ArrayList<>(Arrays.asList(1, 3, 4, 2, 7, 3, 2, 16, 7, 4, 2, 111)));
+        System.out.println(bc.bubbleSort("DESC"));
+    }
+}
--- src/controller/HeapController.java
+++ src/controller/HeapController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HeapController {
@@ -7,8 +9,8 @@ public class HeapController {
         for (int i = (arr.size() - 1) / 2; i >= 0; i--) {
             buildMaxHeap(arr, i, arr.size() - 1);
         }
+        System.out.println(arr);
     }
-
     public void buildMaxHeap(List<Integer> arr, int i, int N) {
         int left = 2 * i;
         int right = 2 * i + 1;
@@ -20,10 +22,13 @@ public class HeapController {
             buildMaxHeap(arr, max, N);
         }
     }
-    public void swap(List<Integer> arr, int i, int j) {
+    public void swap(List<Integer> arr, int i, int max) {
         int tmp = arr.get(i);
-        arr.set(i, arr.get(j));
-        arr.set(j, tmp);
+        arr.set(i, arr.get(max));
+        arr.set(max, tmp);
     }
-
+    public static void main(String[] args) {
+        HeapController hc = new HeapController();
+        hc.buildHeap(new ArrayList<>(Arrays.asList(5,9,2,6,5,7,1)));
+    }
 }
